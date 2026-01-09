91c1b83042a994439642695eb50e643952515834
melio0504 <82772382+melio0504@users.noreply.github.com>
2026-01-09 14:54:11 +0800

    Add webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: { 
+    app: "./src/index.js"
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: "./index.html"
+      })
+    ],
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./index.html"]
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader"
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource"
+      },
+    ],
+  },
+}
