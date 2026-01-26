b85e52e9b1ae3c3a6a6a5c52003584b2b00fafee
melio0504 <82772382+melio0504@users.noreply.github.com>
2026-01-26 14:38:12 +0800

    Display current time in the header

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import './styles.css';
+import { format } from 'date-fns';
+
+// Display current time (AM/PM)
+(function displayTime() {
+  const currentTime = document.querySelector('#currentTime');
+
+  const updateTime = () => {
+    currentTime.textContent = format(new Date(), 'p');
+  }
+
+  updateTime();
+
+  setInterval(displayTime, 60000);
+})();
